4605a48797f3c9ca9e76cb1d59534ae847b5ec29
phamBx2 <46103312+bangpham280296@users.noreply.github.com>
2026-07-14 14:25:41 +0700

    Fix - Remove unused React import from VoucherifyPushModal

--- src/components/voucherify/VoucherifyPushModal.tsx
+++ src/components/voucherify/VoucherifyPushModal.tsx
@@ -1,7 +1,7 @@
 // src/components/voucherify/VoucherifyPushModal.tsx
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "sonner";
 import ProductSearchInput from "./ProductSearchInput";
 import type {
